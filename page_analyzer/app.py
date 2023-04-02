1c83bc71885c628bc33760ed8c724c5f3c9bbd9b
David-Roklem <roklem@mail.ru>
2023-04-02 09:36:25 +0300

    first step

---
+++ page_analyzer/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+app = Flask(__name__)
+app.config.update(SECRET_KEY=SECRET_KEY)
+
+
+@app.route('/')
+def index():
+    '<h1>Hello, the third project. Be kind to your lord!</h1>'
